9772c4a7bb5aba7fc0b2121181675356decc46bd
Mohmaed-Echbiy <echbiymed@gmail.com>
2023-04-14 22:34:56 +0000

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header: FC = () => {
   return (
     <header className='w-full h-[80px] flex justify-center items-center mb-12'>
       <div className='navbar'>
-        <a className='btn btn-ghost normal-case text-xl mx-auto'>DRAG IT</a>
+        <a className='btn btn-ghost normal-case text-xl mx-auto'>DRAG IN</a>
       </div>
     </header>
   );
